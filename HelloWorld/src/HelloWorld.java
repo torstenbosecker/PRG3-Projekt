73a9b9dcdbc7e6d0c1fdd18a803380c786bca1ea
torstenbosecker <erfurtai9>
2014-11-30 15:41:00 +0100

    Erster Versuch (HelloWorld)

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by torsten on 30.11.2014.
+ */
+public class HelloWorld {
+    /**
+     * Schreibt "Hello, world" auf die Konsole.
+     *
+     * @param args Kommandozeilenparameter werden ignoriert
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
